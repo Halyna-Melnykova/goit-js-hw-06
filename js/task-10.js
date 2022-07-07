b610ca8109782e0e0384c876bc361961332fc834
Halyna Melnykova <galyamelnykova@gmail.com>
2022-07-07 14:46:05 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,27 +1,34 @@
 const createBtnEl = document.querySelector('[data-create]');
 const destroyBtnEl = document.querySelector('[data-destroy]');
 const inputEl = document.querySelector('input');
-// console.log(inputEl)
 const newBoxesEl = document.querySelector('#boxes');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function createBoxes(amount) {
+    // let size = 30;
+    amount = Number(inputEl.value);
 
-const createBoxes = amount => {
-   
-    // console.log(i)
-    const color = getRandomHexColor();
-    const boxEl = document.createElement('div');
-    newBoxesEl.append(boxEl);
-    
-    boxEl.style.width = '30px';
-    boxEl.style.height = '30px';
-    boxEl.style.backgroundColor = color;
+    for (let i = 0; i < amount; i += 1) {
+
+      const color = getRandomHexColor();
+
+      const boxEl = document.createElement('div');
+      const size = 30 + i * 10;
+      
+      boxEl.style.width = size + 'px';
+      boxEl.style.height = size + 'px';
+      boxEl.style.backgroundColor = color;
+
+      newBoxesEl.append(boxEl);
+    }
   }
 
+function destroyBoxes() {
+  newBoxesEl.innerHTML = '';
+}
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
 
-createBtnEl.addEventListener('click', createBoxes)
-
-// for (let i = 0; i < amount; i += 1)
